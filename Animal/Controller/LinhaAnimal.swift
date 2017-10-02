91f1ed5ed9aba61697c865a3a1a48e831bcaf55a
Icaro Laudade <icarolaudade@Icaros-Mac.local>
2017-10-02 00:16:34 -0300

    Segundo Commit

--- Animal/Controller/LinhaAnimal.swift
+++ Animal/Controller/LinhaAnimal.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LinhaAnimal: UITableViewCell {
 
+    @IBOutlet weak var familiaAnimal: UILabel!
+    @IBOutlet weak var nomeAnimal: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
